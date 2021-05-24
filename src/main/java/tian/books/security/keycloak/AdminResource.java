293dd0036456e2f057d7b886770e3de0d0864728
tian bai <skynapier@outlook.com>
2021-05-24 22:43:29 +1200

    add simple keycloak

---
+++ src/main/java/tian/books/security/keycloak/AdminResource.java
@@ -0,0 +1,17 @@
+package tian.books.security.keycloak;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.security.Authenticated;
+
+@Path("/api/admin")
+@Authenticated
+public class AdminResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String admin() {
+        return "granted";
+    }
+}
